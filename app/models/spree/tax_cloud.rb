1c5b6ebf3b35a67d558cf21220e3441d21844e2e
Jerrold Thompson <jet@whidbey.com>
2013-01-10 09:52:55 -0800

    Revert to hard code

--- app/models/spree/tax_cloud.rb
+++ app/models/spree/tax_cloud.rb
@@ -33,8 +33,19 @@ module Spree
                               'cartItems' => {'CartItem' => tax_cloud_transaction.cart_items.map(&:to_hash)},
 
                               
-                             'origin' => JSON.parse(Spree::Config.preferred_tax_cloud_origin), 
+                             # 'origin' => JSON.parse(Spree::Config.preferred_tax_cloud_origin), 
+			      'origin' => { 'Address1' =>  "P.O. Box 944" ,
 
+                                                               'Address2' =>  nil ,
+
+                                                               'City' =>  "Langley",
+
+                                                               'State' =>  "Wa",
+
+                                                               'Zip5' =>  "98260" ,
+
+                                                               'Zip4' =>  nil  }, 
+	 }
                              'destination' => destination_address(order.ship_address)
                     
                            })
