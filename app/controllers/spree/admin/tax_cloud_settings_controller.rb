713549435c6e5ad5a0a2d57bb7f91688cb2b632f
Jerrold Thompson <jet@whidbey.com>
2013-01-12 11:09:00 -0800

    TaxCloud Admin

--- app/controllers/spree/admin/tax_cloud_settings_controller.rb
+++ app/controllers/spree/admin/tax_cloud_settings_controller.rb
@@ -8,19 +8,16 @@ module Spree
     def update
       origin = params[:address]
       taxpref = params[:settings]
-      Spree::Config.set(:taxcloud_origin => {:Address1 =>  origin[:taxcloud_address1],
+      Spree::Config.taxcloud_origin = {:Address1 =>  origin[:taxcloud_address1],
 					    :Address2 => origin[:taxcloud_address2],
 					    :City => origin[:taxcloud_city],
 					    :State => origin[:taxcloud_state],
-					    :Zip5 => origin[:taxcloud_zip5] }.to_json  )
+					    :Zip5 => origin[:taxcloud_zip5] }.to_json  
 
       Spree::Config.taxcloud_api_login_id = taxpref[:taxcloud_api_login_id] 
       Spree::Config.taxcloud_api_key = taxpref[:taxcloud_api_key] 
       Spree::Config.taxcloud_product_tic = taxpref[:taxcloud_product_tic] 
       Spree::Config.taxcloud_shipping_tic = taxpref[:taxcloud_shipping_tic] 
-
-      # Spree::Config.set(params[:preferences])
-
 
       respond_to do |format|
 	format.html {
