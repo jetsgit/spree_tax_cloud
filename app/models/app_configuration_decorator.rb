8bdc739c7e5c310c17384b4572fdeeda05854223
Jerrold Thompson <jet@whidbey.com>
2013-01-12 08:31:07 -0800

    Removed preferred getter from app_configuration_decoratlor

--- app/models/app_configuration_decorator.rb
+++ app/models/app_configuration_decorator.rb
@@ -1,8 +1,8 @@
 Spree::AppConfiguration.class_eval do
-  preference :preferred_taxcloud_api_login_id, :string 
-  preference :preferred_taxcloud_api_key, :string  
+  preference :taxcloud_api_login_id, :string 
+  preference :taxcloud_api_key, :string  
   preference :taxcloud_origin, :string, :default => {}
-  preference :preferred_taxcloud_product_tic, :string, :default => '20020'
-  preference :preferred_taxcloud_shipping_tic, :string, :default => '11010'
+  preference :taxcloud_product_tic, :string
+  preference :taxcloud_shipping_tic, :string
 end
 
