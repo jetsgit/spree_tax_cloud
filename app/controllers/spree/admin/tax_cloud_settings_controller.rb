1c53f0af7f9c8bc1817eae9bcd29dc35504248f2
Jerrold Thompson <jet@whidbey.com>
2013-01-12 08:50:47 -0800

    Set taxcloud params individually

--- app/controllers/spree/admin/tax_cloud_settings_controller.rb
+++ app/controllers/spree/admin/tax_cloud_settings_controller.rb
@@ -7,18 +7,19 @@ module Spree
 
     def update
       origin = params[:address]
+      taxcloud = params[:preferences]
       Spree::Config.set(:taxcloud_origin => {:Address1 =>  origin[:taxcloud_address1],
 					    :Address2 => origin[:taxcloud_address2],
 					    :City => origin[:taxcloud_city],
 					    :State => origin[:taxcloud_state],
 					    :Zip5 => origin[:taxcloud_zip5] }.to_json  )
 
-      # Spree::Config.set(:taxcloud_api_login_id => origin[:taxcloud_api_login_id])
-      # Spree::Config.set(:taxcloud_api_key => origin[:taxcloud_api_key])
-      # Spree::Config.set(:taxcloud_product_tic => origin[:taxcloud_product_tic])
-      # Spree::Config.set(:taxcloud_shipping_tic => origin[:taxcloud_shipping_tic])
+      Spree::Config.set(:taxcloud_api_login_id => taxcloud[:taxcloud_api_login_id])
+      Spree::Config.set(:taxcloud_api_key => taxcloud[:taxcloud_api_key])
+      Spree::Config.set(:taxcloud_product_tic => taxcloud[:taxcloud_product_tic])
+      Spree::Config.set(:taxcloud_shipping_tic => taxcloud[:taxcloud_shipping_tic])
 
-      Spree::Config.set(params[:preferences])
+      # Spree::Config.set(params[:preferences])
 
 
       respond_to do |format|
--- app/models/app_configuration_decorator.rb
+++ app/models/app_configuration_decorator.rb
@@ -1,8 +1,8 @@
 Spree::AppConfiguration.class_eval do
   preference :taxcloud_api_login_id, :string 
   preference :taxcloud_api_key, :string  
-  preference :taxcloud_origin, :string, :default => {}
   preference :taxcloud_product_tic, :string
   preference :taxcloud_shipping_tic, :string
+  preference :taxcloud_origin, :string, :default => {}
 end
 
