30e5d0080588774c90e1a67631b4f72172cf0a2b
Jerrold Thompson <jet@whidbey.com>
2013-01-12 08:20:52 -0800

    Added address[] for origin in edit.html.erb

--- app/controllers/spree/admin/tax_cloud_settings_controller.rb
+++ app/controllers/spree/admin/tax_cloud_settings_controller.rb
@@ -6,17 +6,20 @@ module Spree
     end
 
     def update
-      origin = params[:preferences]
+      origin = params[:address]
       Spree::Config.set(:taxcloud_origin => {:Address1 =>  origin[:taxcloud_address1],
 					    :Address2 => origin[:taxcloud_address2],
 					    :City => origin[:taxcloud_city],
 					    :State => origin[:taxcloud_state],
 					    :Zip5 => origin[:taxcloud_zip5] }.to_json  )
 
-      Spree::Config.set(:taxcloud_api_login_id => origin[:taxcloud_api_login_id])
-      Spree::Config.set(:taxcloud_api_key => origin[:taxcloud_api_key])
-      Spree::Config.set(:taxcloud_product_tic => origin[:taxcloud_product_tic])
-      Spree::Config.set(:taxcloud_shipping_tic => origin[:taxcloud_shipping_tic])
+      # Spree::Config.set(:taxcloud_api_login_id => origin[:taxcloud_api_login_id])
+      # Spree::Config.set(:taxcloud_api_key => origin[:taxcloud_api_key])
+      # Spree::Config.set(:taxcloud_product_tic => origin[:taxcloud_product_tic])
+      # Spree::Config.set(:taxcloud_shipping_tic => origin[:taxcloud_shipping_tic])
+
+      Spree::Config.set(params[:preferences])
+
 
       respond_to do |format|
 	format.html {
--- app/models/app_configuration_decorator.rb
+++ app/models/app_configuration_decorator.rb
@@ -1,8 +1,8 @@
 Spree::AppConfiguration.class_eval do
-  preference :taxcloud_api_login_id, :string, :default => '' 
-  preference :taxcloud_api_key, :string, :default => ''  
+  preference :preferred_taxcloud_api_login_id, :string 
+  preference :preferred_taxcloud_api_key, :string  
   preference :taxcloud_origin, :string, :default => {}
-  preference :taxcloud_product_tic, :string, :default => '20020'
-  preference :taxcloud_shipping_tic, :string, :default => '11010'
+  preference :preferred_taxcloud_product_tic, :string, :default => '20020'
+  preference :preferred_taxcloud_shipping_tic, :string, :default => '11010'
 end
 
