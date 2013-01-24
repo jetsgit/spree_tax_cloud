00f2ea15ef49c664fa9d0f9a7bd984dcca27b45e
Jerrold Thompson <jet@whidbey.com>
2013-01-24 14:10:22 -0800

    Adding state transition to :do =>Spree::Calculator::PromotionTax.calcutate

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -8,7 +8,11 @@ Spree::Order.class_eval do
    self.state_machine.after_transition :to => :payment,
 					      :do => :lookup_tax_cloud,
 					      :if => :tax_cloud_eligible?
+
+   self.state_machine.after_transition :to => :payment, 
   
+					      :do => Spree::Calculator::PromotionTax.compute(self)
+
    self.state_machine.after_transition :to => :complete,
 					     :do => :capture_tax_cloud,
 					     :if => :tax_cloud_eligible?
