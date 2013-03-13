41e9d4836d192d420c1dbf15e49ce3f111869fd2
Jerrold Thompson <jet@whidbey.com>
2013-03-13 11:09:14 -0700

    Version 0.1.19, fix update_adjustment, order_decorator.rb

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -10,9 +10,6 @@ Spree::Order.class_eval do
 					      :do => :lookup_tax_cloud,
 					      :if => :tax_cloud_eligible?
 
-   self.state_machine.after_transition :to => :payment, 
-  
-					      :do => :create_tax_charge!
    
    self.state_machine.after_transition :to => :complete,
 					     :do => :capture_tax_cloud,
@@ -32,33 +29,38 @@ Spree::Order.class_eval do
 
 	tax_cloud_transaction.lookup
 
-	Spree::Adjustment.where("originator_id = ?", tax_cloud_transaction.id).update!
-
       else
 
 	 create_tax_cloud_transaction
 
 	 tax_cloud_transaction.lookup
 
-	 adjustments.create do |adjustment|
-
-	    adjustment.source = self
-
-	    adjustment.originator = tax_cloud_transaction
-
-	    adjustment.label = 'Tax'
-
-	    adjustment.mandatory = true
-
-	    adjustment.eligible = true
-
-	    adjustment.amount = tax_cloud_transaction.amount 
-
-	 end
+	 tax_cloud_adjustment
 
       end
 
    end
+   
+   def tax_cloud_adjustment
+
+      adjustments.create do |adjustment|
+
+	 adjustment.source = self
+
+	 adjustment.originator = tax_cloud_transaction
+
+	 adjustment.label = 'Tax'
+
+	 adjustment.mandatory = true
+
+	 adjustment.eligible = true
+
+	 adjustment.amount = tax_cloud_transaction.amount 
+
+      end
+   end
+    
+
 
  
    def capture_tax_cloud
@@ -68,6 +70,23 @@ Spree::Order.class_eval do
       tax_cloud_transaction.capture
 
    end
+
+   def tax_cloud_total(order)
+
+     line_items_total = order.line_items.sum(&:total)
+
+     cloud_rate = order.tax_cloud_transaction.amount / ( line_items_total + order.ship_total )  
+
+     adjusted_total = line_items_total + order.promotions_total 
+
+     round_to_two_places( adjusted_total * cloud_rate ) 
+      
+   end
+
+   def round_to_two_places(amount)
+     BigDecimal.new(amount.to_s).round(2, BigDecimal::ROUND_HALF_UP)
+   end
+
 
    def update_with_taxcloudlookup 
 
@@ -81,8 +100,8 @@ Spree::Order.class_eval do
 
    end
 
-   alias_method :update_without_taxcloud_lookup, :update! 
-   alias_method :update!, :update_with_taxcloudlookup 
+   # alias_method :update_without_taxcloud_lookup, :update! 
+   # alias_method :update!, :update_with_taxcloudlookup 
 
 
 
