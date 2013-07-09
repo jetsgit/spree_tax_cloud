d3437c4cb011d5eec4c838309f1d5530b7005840
Brian Buchalter <brianb@endpoint.com>
2013-07-09 10:10:39 -0400

    Remove Spree::Order methods made obsolete by state machine

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -91,24 +91,4 @@ Spree::Order.class_eval do
     
     promotions.map(&:amount).sum 
   end
-
-
-
-  def update_with_taxcloudlookup 
-
-    unless tax_cloud_transaction.nil?
-
-      tax_cloud_transaction.lookup
-
-    end
-
-    update_without_taxcloud_lookup 
-
-  end
-
-   # alias_method :update_without_taxcloud_lookup, :update! 
-   # alias_method :update!, :update_with_taxcloudlookup 
-
-
-
 end
