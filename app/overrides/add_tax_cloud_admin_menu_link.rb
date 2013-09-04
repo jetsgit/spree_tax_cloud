237e5e4e70db81ef43738675d73e38641745416a
brchristian <brchristian@gmail.com>
2013-09-04 13:50:51 -0700

    Addressing warning message by adding :original for add_tax_cloud_admin_menu
    Addressing issue:
    
    No :original defined for 'add_tax_cloud_admin_menu_link', you should change its definition to include:
     :original => '8e7dd42cfde1795276b04835e0c2b9948a7cadaf'

--- app/overrides/add_tax_cloud_admin_menu_link.rb
+++ app/overrides/add_tax_cloud_admin_menu_link.rb
@@ -3,5 +3,6 @@ Deface::Override.new(:virtual_path  => 'spree/admin/shared/_configuration_menu',
                      :insert_bottom => "[data-hook='admin_configurations_sidebar_menu']",
                      :text => %q{
                         <%= configurations_sidebar_menu_item 'Taxcloud Settings', admin_tax_cloud_settings_path%>
-                     })
+                     },
+                     :original => '8e7dd42cfde1795276b04835e0c2b9948a7cadaf')
 
