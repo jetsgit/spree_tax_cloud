84948535ffdf1bc0dfa3b2be6ee8f2397ab7cb28
Jeff Dutil <JDutil@BurlingtonWebApps.com>
2014-04-10 00:18:10 +0100

    Spree 2.2.x updates but still calculates on order rather than line items...

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -23,7 +23,7 @@ Spree::Order.class_eval do
   def tax_cloud_adjustment
     adjustments.create do |adjustment|
       adjustment.source = self
-      adjustment.originator = tax_cloud_transaction
+      adjustment.source = tax_cloud_transaction
       adjustment.label = 'Tax'
       adjustment.mandatory = true
       adjustment.eligible = true
