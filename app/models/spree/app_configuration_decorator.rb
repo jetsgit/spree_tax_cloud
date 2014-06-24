e5a80b5bbccb680ff62eb183f750635cd4263673
Brian Christian <brchristian@gmail.com>
2014-06-23 17:15:43 -0700

    rename taxcloud_product_tic to taxcloud_default_product_tic

--- app/models/spree/app_configuration_decorator.rb
+++ app/models/spree/app_configuration_decorator.rb
@@ -1,7 +1,7 @@
 Spree::AppConfiguration.class_eval do
   preference :taxcloud_api_login_id, :string
   preference :taxcloud_api_key, :string
-  preference :taxcloud_product_tic, :string, default: '00000'
+  preference :taxcloud_default_product_tic, :string, default: '00000'
   preference :taxcloud_shipping_tic, :string, default: '11010'
   preference :taxcloud_usps_user_id, :string
   preference :taxcloud_origin, :string, :default => {}.to_json
