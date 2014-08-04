c1a2640252c4343938af7ac6b8e217b1e96aced3
Jerrold Thompson <jet@whidbey.com>
2014-08-03 22:57:07 -0700

    Return Merchandise Authorization implementation

---
+++ app/models/spree/return_authorization_decorator.rb
@@ -0,0 +1,34 @@
+module Spree 
+  ReturnAuthorization.class_eval do
+    def process_return_with_taxcloud_return
+      transaction = Spree::TaxCloudTransaction.transaction_for_return(order)
+      iu_groups = inventory_units.group_by(&:variant_id)
+      index = -1
+      iu_groups.each do |key,value|
+        quantity = value.count
+        transaction.cart_items << Spree::TaxCloudTransaction.cart_item_from_return(Spree::LineItem.find(value.first.line_item_id), quantity, index += 1) 
+      end     
+      response = transaction.lookup
+      if !response.blank?
+        response_cart_items = response.cart_items
+        tax_return_amount = 0.00 
+        response_cart_items.each do |cart_item|
+          tax_return_amount += round_to_two_places( cart_item.tax_amount )
+        end
+        transaction.returned 
+        Adjustment.create(adjustable: order, amount: tax_return_amount.abs * -1, label: Spree.t(:rma_tax_credit), source: self)
+        order.update! 
+      else
+        raise ::SpreeTaxCloud::Error, 'TaxCloud response unsuccessful!'
+      end
+      process_return_without_taxcloud_return 
+    end
+
+    alias_method_chain :process_return, :taxcloud_return
+
+    def round_to_two_places(amount)
+      BigDecimal.new(amount.to_s).round(2, BigDecimal::ROUND_HALF_UP)
+    end
+     
+  end
+end
--- app/models/spree/tax_cloud_transaction.rb
+++ app/models/spree/tax_cloud_transaction.rb
@@ -1,11 +1,11 @@
 require_dependency 'spree/order'
 
 module Spree
-  class TaxCloudTransaction < ActiveRecord::Base
+	class TaxCloudTransaction < ActiveRecord::Base
 
-    belongs_to :order
+		belongs_to :order
 
-    validates :order, :presence => true
+		validates :order, :presence => true
 
 		def self.transaction_from_order(order)
 			stock_location = Spree::StockLocation.active.where("city IS NOT NULL and state_id IS NOT NULL").first
@@ -28,49 +28,86 @@ module Spree
 			return transaction
 		end
 
-    def self.address_from_spree_address(address)
-      ::TaxCloud::Address.new(
-      address1:   address.address1,
-      address2:   address.address2,
-      city:       address.city,
-      state:      address.try(:state).try(:abbr), # replace with state_text if possible
-      zip5:       address.zipcode[0...5]
-      )
-    end
-    def self.cart_item_from_item(item, index)
-      if item.class.to_s == "Spree::LineItem"
-        line_item = item
-        ::TaxCloud::CartItem.new(
-        index:      index,
-        item_id:    line_item.try(:variant).try(:sku) || "LineItem " + line_item.id.to_s,
-        tic:        line_item.product.tax_cloud_tic,
-        price:      ( line_item.quantity * line_item.price + line_item.promo_total) / line_item.quantity,
-        quantity:   line_item.quantity
-        )
+		def self.transaction_for_return(order)
+			binding.pry
+			stock_location = Spree::StockLocation.active.where("city IS NOT NULL and state_id IS NOT NULL").first
 
-      elsif item.class.to_s == "Spree::Shipment"
-        shipment = item
-        ::TaxCloud::CartItem.new(
-        index:      index,
-        item_id:    "Shipment " + shipment.number,
-        tic:        Spree::Config.taxcloud_shipping_tic,
-        price:      shipment.cost,
-        quantity:   1
-        )
+			unless stock_location
+				raise 'Please ensure you have at least one Stock Location with a valid address for your tax origin.'
+			end
 
-      else
-        raise 'TaxCloud::CartItem cannot be made from this item.'
-      end
-    end
+			transaction = ::TaxCloud::Transaction.new(
+				customer_id: order.user_id || order.email,
+				order_id: order.number,
+				cart_id: order.number,
+				origin: address_from_spree_address(stock_location),
+				destination: address_from_spree_address(order.ship_address)
+			)
 
-    def self.shipping_item_from_order(order, index)
-      ::TaxCloud::CartItem.new(
-      index:      index,
-      item_id:    "SHIPPING",
-      tic:        Spree::Config.taxcloud_shipping_tic,
-      price:      order.ship_total,
-      quantity:   1
-      )
-    end    
-  end
+			return transaction
+		end			 
+
+
+
+		def self.address_from_spree_address(address)
+			::TaxCloud::Address.new(
+				address1:   address.address1,
+				address2:   address.address2,
+				city:       address.city,
+				state:      address.try(:state).try(:abbr), # replace with state_text if possible
+				zip5:       address.zipcode[0...5]
+			)
+		end
+
+		def self.cart_item_from_item(item, index)
+			if item.class.to_s == "Spree::LineItem"
+				line_item = item
+				::TaxCloud::CartItem.new(
+					index:      index,
+					item_id:    line_item.try(:variant).try(:sku) || "LineItem " + line_item.id.to_s,
+					tic:        line_item.product.tax_cloud_tic,
+					price:      ( line_item.quantity * line_item.price + line_item.promo_total) / line_item.quantity,
+					quantity:   line_item.quantity
+				)
+
+			elsif item.class.to_s == "Spree::Shipment"
+				shipment = item
+				::TaxCloud::CartItem.new(
+					index:      index,
+					item_id:    "Shipment " + shipment.number,
+					tic:        Spree::Config.taxcloud_shipping_tic,
+					price:      shipment.cost,
+					quantity:   1
+				)
+
+			else
+				raise 'TaxCloud::CartItem cannot be made from this item.'
+			end
+		end
+
+		def self.cart_item_from_return(line_item, quantity, index)
+			if line_item
+				::TaxCloud::CartItem.new(
+					index:      index,
+					item_id:    line_item.try(:variant).try(:sku) || "LineItem " + line_item.id.to_s,
+					tic:        line_item.product.tax_cloud_tic,
+					price:      ( line_item.quantity * line_item.price + line_item.promo_total) / line_item.quantity,
+					quantity:   quantity
+				)
+			else
+				raise 'TaxCloud::CartItem cannot be made from this item.'
+			end
+		end
+
+
+		def self.shipping_item_from_order(order, index)
+			::TaxCloud::CartItem.new(
+				index:      index,
+				item_id:    "SHIPPING",
+				tic:        Spree::Config.taxcloud_shipping_tic,
+				price:      order.ship_total,
+				quantity:   1
+			)
+		end    
+	end
 end
