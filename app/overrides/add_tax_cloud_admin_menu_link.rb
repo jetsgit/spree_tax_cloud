eabba79c321d3392e4e7d88be45171cefaed7f96
removedporn <86824510+removedporn@users.noreply.github.com>
2021-11-02 20:24:54 +0800

    Delete app directory

--- app/overrides/add_tax_cloud_admin_menu_link.rb
+++
@@ -1,7 +0,0 @@
-Deface::Override.new(
-  virtual_path:  "spree/admin/shared/_configuration_menu",
-  name:          "add_tax_cloud_admin_menu_link",
-  insert_bottom: "[data-hook='admin_configurations_sidebar_menu']",
-  text:          "<%= configurations_sidebar_menu_item 'Taxcloud Settings', edit_admin_tax_cloud_settings_path %>"
-)
-
